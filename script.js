42ee6644f2a534c98c2e0dca71a2f684ca94a09a
Rafał Dadas <dadasrafal@gmail.com>
2024-10-24 14:18:20 +0200

    Working logic & basic styling

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
-const result = document.getElementById("result");
+const textInput = document.getElementById('text-input');
+const checkButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
 
 const isInputEmpty = (input) => {
   return input === '';
 };
 
 const removeNonAlphanumericCharacters = (input) => {
-  const regex = /[A-Z][a-z]\d/gi;
-  console.log(input.replace(regex, ""));
-  return input.replace(regex, "");
+  const regex = /[\s,.\-_+=*/\\?';:\]\[{}!@#$%^&()`"<>]+/gi;
+  return input.replace(regex, '');
 };
 
 const isPalindrome = () => {
   const input = textInput.value;
-  const filteredInput = removeNonAlphanumericCharacters(input);
-  for (let i = 0; i < filteredInput.length; i++) {
-    console.log(filteredInput[i]);
+  const filteredInput = removeNonAlphanumericCharacters(input).toLowerCase();
+  let reversedInput = '';
+  for (let i = filteredInput.length; i > 0; i--) {
+    reversedInput += filteredInput[i - 1];
   }
+  return filteredInput === reversedInput;
 };
 
 const checkForPalindrome = () => {
   if (isInputEmpty(textInput.value)) {
-    alert("Please input a value");
+    alert('Please input a value');
     return null;
   }
-  isPalindrome();
-  result.classList.remove("hide");
+  result.innerText = isPalindrome()
+      ? `${textInput.value} is a palindrome`
+      : `${textInput.value} is not a palindrome`;
+  result.classList.remove('hide');
 };
 
-checkButton.addEventListener("click", checkForPalindrome);
+checkButton.addEventListener('click', checkForPalindrome);
